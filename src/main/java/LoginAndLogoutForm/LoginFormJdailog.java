f36630e58f20a5527da71a81dfc126f9f9a30662
FRONT-END.06 <danin9733@gmail.com>
2024-05-08 10:20:13 +0700

    Update

--- src/main/java/LoginAndLogoutForm/LoginFormJdailog.java
+++ src/main/java/LoginAndLogoutForm/LoginFormJdailog.java
@@ -355,7 +355,7 @@ public class LoginFormJdailog extends javax.swing.JDialog {
 //         String userId = txtUserId.getValueTextField();
 //         String password = txtPassword.getValuePassword();
 
-         String userId = "0005";
+         String userId = "0002";
          String password = "TT@126$kh#";
          String deviceName = JavaConstant.getDeviceName();
 
