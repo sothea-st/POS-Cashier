f77043b8152aa3cce0fb659f3da758c9d95cdd44
FRONT-END.06 <danin9733@gmail.com>
2024-05-10 13:43:21 +0700

    Update

--- src/main/java/Return/PrinterReturn.java
+++ src/main/java/Return/PrinterReturn.java
@@ -751,7 +751,7 @@ public class PrinterReturn extends javax.swing.JDialog {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 754, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGap(0, 0, 0)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
