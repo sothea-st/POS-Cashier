48371269c6d77a4a4b8d9b868a681fb06355a6da
sothea kam <sotheakam210@gmail.com>
2024-05-11 08:04:23 +0700

    update ui

--- src/main/java/View/MainPage/MainPage.java
+++ src/main/java/View/MainPage/MainPage.java
@@ -42,7 +42,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+ 
 import javax.swing.JPanel;
 import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
