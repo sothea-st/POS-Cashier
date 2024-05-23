215c35a2782fa49650995b45e521f1d9925a213f
MOBILE-APP.02 <MOBILE-APP.02@TURBOTECH.COM>
2024-05-23 16:08:06 +0700

    upate

--- src/main/java/Components/JavaAlertMessage.java
+++ src/main/java/Components/JavaAlertMessage.java
@@ -2,13 +2,17 @@ package Components;
 
 import Color.WindowColor;
 import Constant.JavaConstant;
+import Event.ButtonEvent;
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+import pdf.PrintPanelToPDF;
 
 public class JavaAlertMessage extends javax.swing.JDialog {
 
      public static String returnMsg = "You are in processing return !";
      public static int countTimeQty = 0;
      private String message;
-   
 
      public JavaAlertMessage(java.awt.Frame parent, boolean modal) {
           super(parent, modal);
@@ -19,89 +23,117 @@ public class JavaAlertMessage extends javax.swing.JDialog {
      }
 
      @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
+     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+     private void initComponents() {
 
-        panelMessage = new javax.swing.JPanel();
-        btnClose = new Button.Button();
-        panelOutSide = new javax.swing.JPanel();
-        lbMessage = new javax.swing.JLabel();
+          panelMessage = new javax.swing.JPanel();
+          btnClose = new Button.Button();
+          panelOutSide = new javax.swing.JPanel();
+          lbMessage = new javax.swing.JLabel();
+          btnBrowse = new Button.Button();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+          setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
-        btnClose.setButtonName("Close");
-        btnClose.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                btnCloseMouseClicked(evt);
-            }
-        });
+          btnClose.setButtonName("Close");
+          btnClose.addMouseListener(new java.awt.event.MouseAdapter() {
+               public void mouseClicked(java.awt.event.MouseEvent evt) {
+                    btnCloseMouseClicked(evt);
+               }
+          });
 
-        panelOutSide.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0), 2));
+          panelOutSide.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0), 2));
 
-        lbMessage.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
-        lbMessage.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        lbMessage.setText("Message");
+          lbMessage.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
+          lbMessage.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+          lbMessage.setText("Message");
 
-        javax.swing.GroupLayout panelOutSideLayout = new javax.swing.GroupLayout(panelOutSide);
-        panelOutSide.setLayout(panelOutSideLayout);
-        panelOutSideLayout.setHorizontalGroup(
-            panelOutSideLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panelOutSideLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(lbMessage, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
-        );
-        panelOutSideLayout.setVerticalGroup(
-            panelOutSideLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panelOutSideLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(lbMessage, javax.swing.GroupLayout.DEFAULT_SIZE, 80, Short.MAX_VALUE)
-                .addContainerGap())
-        );
+          javax.swing.GroupLayout panelOutSideLayout = new javax.swing.GroupLayout(panelOutSide);
+          panelOutSide.setLayout(panelOutSideLayout);
+          panelOutSideLayout.setHorizontalGroup(
+               panelOutSideLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+               .addGroup(panelOutSideLayout.createSequentialGroup()
+                    .addContainerGap()
+                    .addComponent(lbMessage, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addContainerGap())
+          );
+          panelOutSideLayout.setVerticalGroup(
+               panelOutSideLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+               .addGroup(panelOutSideLayout.createSequentialGroup()
+                    .addContainerGap()
+                    .addComponent(lbMessage, javax.swing.GroupLayout.DEFAULT_SIZE, 80, Short.MAX_VALUE)
+                    .addContainerGap())
+          );
 
-        javax.swing.GroupLayout panelMessageLayout = new javax.swing.GroupLayout(panelMessage);
-        panelMessage.setLayout(panelMessageLayout);
-        panelMessageLayout.setHorizontalGroup(
-            panelMessageLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panelMessageLayout.createSequentialGroup()
-                .addGroup(panelMessageLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelMessageLayout.createSequentialGroup()
-                        .addContainerGap(281, Short.MAX_VALUE)
-                        .addComponent(btnClose, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(panelMessageLayout.createSequentialGroup()
-                        .addGap(20, 20, 20)
-                        .addComponent(panelOutSide, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                .addGap(20, 20, 20))
-        );
-        panelMessageLayout.setVerticalGroup(
-            panelMessageLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panelMessageLayout.createSequentialGroup()
-                .addGap(20, 20, 20)
-                .addComponent(panelOutSide, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 126, Short.MAX_VALUE)
-                .addComponent(btnClose, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(20, 20, 20))
-        );
+          btnBrowse.setBackground(new java.awt.Color(47, 155, 70));
+          btnBrowse.setButtonName("Browse");
+          btnBrowse.addMouseListener(new java.awt.event.MouseAdapter() {
+               public void mouseClicked(java.awt.event.MouseEvent evt) {
+                    btnBrowseMouseClicked(evt);
+               }
+          });
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(panelMessage, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(panelMessage, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
+          javax.swing.GroupLayout panelMessageLayout = new javax.swing.GroupLayout(panelMessage);
+          panelMessage.setLayout(panelMessageLayout);
+          panelMessageLayout.setHorizontalGroup(
+               panelMessageLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+               .addGroup(panelMessageLayout.createSequentialGroup()
+                    .addGroup(panelMessageLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                         .addGroup(panelMessageLayout.createSequentialGroup()
+                              .addContainerGap(164, Short.MAX_VALUE)
+                              .addComponent(btnClose, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE)
+                              .addGap(18, 18, 18)
+                              .addComponent(btnBrowse, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE))
+                         .addGroup(panelMessageLayout.createSequentialGroup()
+                              .addGap(20, 20, 20)
+                              .addComponent(panelOutSide, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                    .addGap(20, 20, 20))
+          );
+          panelMessageLayout.setVerticalGroup(
+               panelMessageLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+               .addGroup(panelMessageLayout.createSequentialGroup()
+                    .addGap(20, 20, 20)
+                    .addComponent(panelOutSide, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 126, Short.MAX_VALUE)
+                    .addGroup(panelMessageLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                         .addComponent(btnClose, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                         .addComponent(btnBrowse, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGap(20, 20, 20))
+          );
 
-        pack();
-        setLocationRelativeTo(null);
-    }// </editor-fold>//GEN-END:initComponents
+          javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+          getContentPane().setLayout(layout);
+          layout.setHorizontalGroup(
+               layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+               .addComponent(panelMessage, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+          );
+          layout.setVerticalGroup(
+               layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+               .addComponent(panelMessage, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+          );
+
+          pack();
+          setLocationRelativeTo(null);
+     }// </editor-fold>//GEN-END:initComponents
 
      private void btnCloseMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnCloseMouseClicked
 
           dispose();
      }//GEN-LAST:event_btnCloseMouseClicked
+
+     private void btnBrowseMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnBrowseMouseClicked
+
+          String filePath = PrintPanelToPDF.folderPath;
+          File file = new File(filePath);
+          if (file.exists()) {
+               try {
+                    Desktop.getDesktop().open(file);
+               } catch (IOException e) {
+                    System.out.println("Error opening file: " + e.getMessage());
+               }
+          } else {
+               System.out.println("File does not exist: " + filePath);
+          }
+     }//GEN-LAST:event_btnBrowseMouseClicked
 
      /**
       * @param args the command line
@@ -156,10 +188,11 @@ public class JavaAlertMessage extends javax.swing.JDialog {
      }
 
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private Button.Button btnClose;
-    private javax.swing.JLabel lbMessage;
-    private javax.swing.JPanel panelMessage;
-    private javax.swing.JPanel panelOutSide;
-    // End of variables declaration//GEN-END:variables
+     // Variables declaration - do not modify//GEN-BEGIN:variables
+     private Button.Button btnBrowse;
+     private Button.Button btnClose;
+     private javax.swing.JLabel lbMessage;
+     private javax.swing.JPanel panelMessage;
+     private javax.swing.JPanel panelOutSide;
+     // End of variables declaration//GEN-END:variables
 }
