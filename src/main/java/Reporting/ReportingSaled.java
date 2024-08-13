f7d793bb2026467b1692cf7d1f2755e40cc0728b
FRONT-END.06 <danin9733@gmail.com>
2024-08-13 17:03:39 +0700

    Update UI

--- src/main/java/Reporting/ReportingSaled.java
+++ src/main/java/Reporting/ReportingSaled.java
@@ -648,7 +648,7 @@ public class ReportingSaled extends javax.swing.JDialog {
                     String.valueOf(String.format("%.2f", detail.getVatAmt())),
                     String.valueOf(String.format("%.2f", detail.getPLT())),
                     String.valueOf(String.format("%.2f", detail.getNetSale())),
-                    String.valueOf(detail.getCost()),
+                    String.valueOf(String.format("%.2f", detail.getCost())),
                     String.valueOf(String.format("%.2f", detail.getMargin())),
                     String.valueOf(detail.getUserName())
                );
