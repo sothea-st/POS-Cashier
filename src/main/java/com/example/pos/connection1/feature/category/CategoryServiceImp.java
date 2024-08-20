78234443038f7fcf7def6ec607431ecef08e6342
sotheakam <MOBILE-APP.02@TURBOTECH.COM>
2024-08-20 08:11:27 +0700

    ad

--- src/main/java/com/example/pos/connection1/feature/category/CategoryServiceImp.java
+++ src/main/java/com/example/pos/connection1/feature/category/CategoryServiceImp.java
@@ -43,11 +43,12 @@ public class CategoryServiceImp implements CategoryService {
 
     @Override
     public JavaCollectionResponse<?> read(int pageNumber, int pageSize, String code) {
-        Sort sortById = Sort.by(Sort.Direction.DESC, "id");
+        Sort sortById = Sort.by(Sort.Direction.ASC, "id");
         PageRequest pageRequest = PageRequest.of(pageNumber, pageSize, sortById);
         Page<Category> pages = categoryRepository.findByCodeAndStatusTrueAndIsDeletedFalse(code, pageRequest);
 
         List<CategoryResponse> data = pages.getContent().stream()
+
                 .map(p -> CategoryResponse.builder()
                         .id(p.getId())
                         .catNameEn(p.getCatNameEn())
