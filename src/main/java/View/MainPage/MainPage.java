ab026eed6daa4daee1717096ece4e76e119893cc
sotheakam <MOBILE-APP.02@TURBOTECH.COM>
2024-11-25 17:19:56 +0700

    update

--- src/main/java/View/MainPage/MainPage.java
+++ src/main/java/View/MainPage/MainPage.java
@@ -3,7 +3,7 @@ package View.MainPage;
 import BlogCode.ActionCloseShift;
 import BlogCode.JavaActionAddHold;
 import BlogCode.JavaActionDiscount;
-import BlogCode.JavaBlogImage;
+
 import BlogCode.JavaEventNextPrevious;
 import BlogCode.JavaExistScreen;
 import BlogCode.JavaSearchByNameAndCode;
