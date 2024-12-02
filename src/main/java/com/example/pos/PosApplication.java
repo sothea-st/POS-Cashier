aec5abf8d6301377aba5fba621540a79170fd0b0
sotheakam <MOBILE-APP.02@TURBOTECH.COM>
2024-12-02 14:56:49 +0700

    update

--- src/main/java/com/example/pos/PosApplication.java
+++ src/main/java/com/example/pos/PosApplication.java
@@ -10,8 +10,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // @CrossOrigin(origins = "http://localhost:3000") 
 // @EnableScheduling
 public class PosApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(PosApplication.class, args);
 	}
+
 
 }
