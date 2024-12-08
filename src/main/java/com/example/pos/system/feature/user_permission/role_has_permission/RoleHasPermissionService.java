c92e46a212c22411f82e7b97acdf830381af53c7
sothea-st <sotheakam210@gmail.com>
2024-12-08 21:34:35 +0700

    update

--- src/main/java/com/example/pos/system/feature/user_permission/role_has_permission/RoleHasPermissionService.java
+++ src/main/java/com/example/pos/system/feature/user_permission/role_has_permission/RoleHasPermissionService.java
@@ -3,10 +3,13 @@ package com.example.pos.system.feature.user_permission.role_has_permission;
 import com.example.pos.system.constant.util.collection_response.JavaCollectionResponse;
 import com.example.pos.system.constant.util.response_success.ResponseSuccess;
 import com.example.pos.system.feature.user_permission.role_has_permission.dto.RoleHasPermissionRequest;
+import com.example.pos.system.feature.user_permission.role_has_permission.dto.RoleHasRequest;
+
+import java.util.List;
 
 public interface RoleHasPermissionService {
 
-    ResponseSuccess create(RoleHasPermissionRequest roleHasPermissionRequest);
+    ResponseSuccess create(RoleHasRequest roleHasRequest);
 
     JavaCollectionResponse<?> readByParentIdAndRoleId(Integer parentId,Integer roleId);
 
